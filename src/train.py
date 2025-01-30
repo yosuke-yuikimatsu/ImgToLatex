98202c4be5defaccb0ff61432075a9f3d8b6b6c2
Dmitry-Geyvandov <geyvandovdd@gmail.com>
2025-01-30 14:33:55 +0000

    16 -> 6

--- src/train.py
+++ src/train.py
@@ -29,7 +29,7 @@ MODEL_SAVE_PATH = Path.cwd() / "models" / "image_to_latex_model.pth"
 os.makedirs(MODEL_SAVE_PATH.parent, exist_ok=True)
 
 # Гиперпараметры
-BATCH_SIZE = 16
+BATCH_SIZE = 6
 NUM_EPOCHS = 10         
 LEARNING_RATE = 1e-4     
 START_TEACHER_FORCING = 0.7  
