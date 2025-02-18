b548a0f141575446965fba2dd76b13ec7e46e327
Sam Tsinman <sptsinman@edu.hse.ru>
2025-02-18 20:37:55 +0300

    substantially refactored dataloader

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,98 +1,127 @@
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
-import matplotlib.pyplot as plt
 from torchvision import transforms
+from PIL import Image
 from pathlib import Path
+import math
+import random
+
+
+def load_labels(label_path):
+    with open(label_path, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file]
+
+
 class DataGen(Dataset):
-    def __init__(self, data_base_dir, data_path, label_path, max_aspect_ratio=None, max_encoder_l_h=None,
-                 max_encoder_l_w=None, max_decoder_l=None):
-        self.data_base_dir = data_base_dir
-        self.data_path = data_path
-        self.label_path = label_path
-        self.max_aspect_ratio = max_aspect_ratio or float('inf')
-        self.max_encoder_l_h = max_encoder_l_h or float('inf')
-        self.max_encoder_l_w = max_encoder_l_w or float('inf')
-        self.max_decoder_l = max_decoder_l or float('inf')
-        # читаем данные
-        with open(self.data_path, 'r') as file:
-            self.lines = [line.strip().split() for line in file.readlines()]
-        with open(self.label_path, 'r') as file:
-            self.latex_lines = file.readlines()
-        self.cursor = 0
+    GO = 1
+    EOS = 2
+
+    def __init__(self, data_base_dir, data_path, label_path, max_aspect_ratio=4.0, max_decoder_l=20,
+                 img_width_range=(12, 320)):
+        self.data_base_dir = Path(data_base_dir)
+        self.max_aspect_ratio = max_aspect_ratio
+        self.max_decoder_l = max_decoder_l
+        self.img_width_range = img_width_range
+        self.labels = load_labels(label_path)
+        with open(data_path, 'r', encoding='utf-8') as file:
+            self.samples = [line.strip().split() for line in file.readlines()]
+        self.bucket_specs = [
+            (int(64 / 4), 9 + 2),
+            (int(108 / 4), 15 + 2),
+            (int(140 / 4), 17 + 2),
+            (int(256 / 4), 20 + 2),
+            (int(math.ceil(img_width_range[1] / 4)), max_decoder_l + 2)
+        ]
+        self.bucket_min_width, self.bucket_max_width = img_width_range
+        self.bucket_data = {i: [] for i in range(self.bucket_max_width + 1)}
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+
     def __len__(self):
-        return len(self.lines)
+        return len(self.samples)
+
     def __getitem__(self, idx):
-        img_path, label_index = self.lines[idx]
-        img = Image.open(os.path.join(self.data_base_dir, img_path)).convert("RGB")
-        label_str = self.latex_lines[int(label_index)].strip()
-        label_list = self._label_to_numeric(label_str) # метки в числа
-        return img, label_list, img_path
+        img_path, label_index = self.samples[idx]
+        img = Image.open(self.data_base_dir / img_path)
+        label = self.labels[int(label_index)]
+        label_tensor = torch.tensor(self._label_to_numeric(label), dtype=torch.long)
+
+        width = img.size[0]
+
+        bucket_idx = min(width, self.bucket_max_width)
+        self.bucket_data[bucket_idx].append((img, label_tensor, img_path))
+
+        return img, label_tensor, img_path
+
     def _label_to_numeric(self, label):
-        # Для правильной генерации надо добавлять Start of Sequence token и End of Sequence token
-        return [129] + [ord(c) for c in label]  + [130]
-# Для корректной обработки батчей с изображениями разного размера
-def collate_fn(batch):
+        label_indices = [self.GO] + [ord(c) for c in label] + [self.EOS]
+        return label_indices[:self.max_decoder_l]  # Усечение длинных меток
+
+    def get_buckets(self, batch_size):
+        for bucket_idx, bucket in self.bucket_data.items():
+            random.shuffle(bucket)
+            for i in range(0, len(bucket), batch_size):
+                batch_data = bucket[i:i + batch_size]
+                images, labels, img_paths = zip(*batch_data)
+
+                max_height = max(img.size[1] for img in images)
+                max_width = max(img.size[0] for img in images)
+
+                images = [pad_and_transform(img, max_width, max_height) for img in images]
+                images = torch.stack(images, 0)
+                labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=0)
+
+                yield images, labels, img_paths
+
+
+def dynamic_collate_fn(batch):
+    batch.sort(key=lambda x: x[0].size[0] / x[0].size[1], reverse=True)
+
     images, targets, img_paths = zip(*batch)
-    #максимальная высота и ширину среди всех изображений в батче
-    max_height = max([img.size[1] for img in images])
-    max_width = max([img.size[0] for img in images])
-    # Преобразуем изображения в одинаковый размер (через паддинг, может, это плохая затея, вдруг нолики в тензоре плохо скажутся на модели)
-    images = [pad_image(img, max_width, max_height) for img in images]
-    images = torch.stack(images, 0)  # Ставим изображения в батч
-    # Преобразуем метки в тензоры, с паддингом для одинаковой длины
-    targets = torch.nn.utils.rnn.pad_sequence([torch.tensor(target) for target in targets], batch_first=True,
-                                              padding_value=0)
+
+    max_height = max(img.size[1] for img in images)
+    max_width = max(img.size[0] for img in images)
+
+    if max_width / max_height > 4.0:
+        max_width = int(max_height * 4.0)
+
+    images = [pad_and_transform(img, max_width, max_height) for img in images]
+    images = torch.stack(images, 0)
+
+    targets = torch.nn.utils.rnn.pad_sequence(targets, batch_first=True, padding_value=0)
+
     return images, targets, img_paths
 
-def pad_image(image, target_width, target_height):
+
+def pad_and_transform(image, target_width, target_height):
     width, height = image.size
-    padding_left = (target_width - width) // 2
-    padding_top = (target_height - height) // 2
-    padding_right = target_width - width - padding_left
-    padding_bottom = target_height - height - padding_top
-    # Добавляем паддинг (слева, сверху, справа, снизу)
-    padded_image = transforms.functional.pad(image, (padding_left, padding_top, padding_right, padding_bottom),
-                                             fill=255)
+    padding = [0, 0, target_width - width, target_height - height]
+    padded_image = transforms.functional.pad(image, padding, fill=255)
     return transforms.ToTensor()(padded_image)
 
-def indices_to_latex(indices):
-    cleaned_indices = [idx for idx in indices if idx != 0]  # Убираем нули
-    return ''.join([chr(idx) for idx in cleaned_indices])
-
-
-
-# пытаемся эстетично визуализировать
-def visualize_batch_with_labels(images, targets, img_paths):
-    images = images.permute(0, 2, 3, 1).cpu().numpy()  # Переводим в numpy
-    batch_size = images.shape[0]
-    num_images = min(batch_size, 4)
-    fig, axes = plt.subplots(1, num_images, figsize=(15, 15))
-    for i in range(num_images):
-        ax = axes[i]
-        ax.imshow(images[i])
-        ax.set_title(f"Image {i + 1}")
-        ax.axis('off')
-        label_str = (targets[i])
-        ax.text(0.5, -0.2, label_str, ha='center', va='top', transform=ax.transAxes, fontsize=5, color='black')
-    plt.tight_layout()
-    plt.show()
 
 def test_loader():
-    # Пути к данным
-    current_dir = Path(__file__).resolve().parent
-    parent_dir = current_dir.parent
-    parent_dir = parent_dir.parent
-    samples_dir = parent_dir / 'samples'
-    data_base_dir = samples_dir / 'images' / 'formula_images_processed'
-    data_path = samples_dir / 'im2latex_train_filter.lst'
-    label_path = samples_dir / 'im2latex_formulas.tok.lst'
 
-    dataset = DataGen(data_base_dir, data_path, label_path)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+    dataset = DataGen(
+        data_base_dir='/Users/semencinman/Documents/GitHub/ImgToLatex/samples/images/formula_images_processed',
+        data_path='/Users/semencinman/Documents/GitHub/ImgToLatex/samples/im2latex_train_filter.lst',
+        label_path='/Users/semencinman/Documents/GitHub/ImgToLatex/samples/im2latex_formulas.tok.lst'
+    )
+
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dynamic_collate_fn)
+
     for images, targets, img_paths in dataloader:
-        visualize_batch_with_labels(images, targets, img_paths)  # Визуализируем батч
+        print("Batch loaded with image paths and annotations:")
+        for img_path, target in zip(img_paths, targets):
+            annotation = ''.join([chr(idx) if idx > 2 else '' for idx in target.tolist()])
+            print(f"Image path: {img_path}, Annotation: {annotation}")
         break
+
 if __name__ == "__main__":
     test_loader()
+
+
