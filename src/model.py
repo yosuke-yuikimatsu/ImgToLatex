083b90f4ffc3172b9f46c420dfb3774527c4906c
Sam Tsinman <sptsinman@edu.hse.ru>
2025-03-15 14:30:31 +0300

    RL included

--- src/model.py
+++ src/model.py
@@ -17,7 +17,7 @@ class ImageToLatexModel(nn.Module):
     ):
         super().__init__()
         self.cnn = CNN(output_channels=enc_hidden_dim)
-        self.encoder = TransformerEncoderModule(enc_hid_dim=enc_hidden_dim, num_layers=10, ffn_dim=8192,num_heads=8)
+        self.encoder = TransformerEncoderModule(enc_hid_dim=enc_hidden_dim, num_layers=10, ffn_dim=8192, num_heads=8)
         self.decoder = TransformerDecoderModule(
             vocab_size=vocab_size,
             embed_dim=enc_hidden_dim,
@@ -26,7 +26,8 @@ class ImageToLatexModel(nn.Module):
             ffn_dim=8192,
             max_length=max_length,
             sos_index=sos_index,
-            eos_index=eos_index
+            eos_index=eos_index,
+            pad_idx=pad_idx  # Добавляем pad_idx в декодер
         )
         self.pad_idx = pad_idx
         self.sos_index = sos_index
@@ -34,14 +35,21 @@ class ImageToLatexModel(nn.Module):
         self.max_length = max_length
         self.beam_width = beam_width
 
-    def forward(self, images, tgt_tokens=None):
+    def forward(self, images, tgt_tokens=None, train=False):
         features = self.cnn(images)
         encoder_outputs = self.encoder(features)
         B, H, W, D = encoder_outputs.shape
         memory = encoder_outputs.view(B, H * W, D)
-        if tgt_tokens is not None:
-            logits = self.decoder(tgt_tokens, memory)
+
+        if train and tgt_tokens is None:
+            # RL режим с REINFORCE
+            predicted_tokens, rewards, loss = self.decoder(tgt_tokens=None, memory=memory, train=True)
+            return predicted_tokens, rewards, loss
+        elif tgt_tokens is not None:
+            # Supervised режим
+            logits = self.decoder(tgt_tokens=tgt_tokens, memory=memory)
             return logits  # В режиме обучения возвращаем только логиты
         else:
-            logits, predicted_tokens = self.decoder(tgt_tokens, memory,self.beam_width)
+            # Инференс режим
+            logits, predicted_tokens = self.decoder(tgt_tokens=None, memory=memory, beam_width=self.beam_width)
             return logits, predicted_tokens  # В режиме инференса возвращаем логиты и токены
